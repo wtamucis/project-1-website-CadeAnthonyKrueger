83d1c783f14f41c7a461177947a56370065f1c30
Cade Krueger <Cadeak22@outlook.com>
2025-11-18 05:44:44 -0600

    Beginning of start page

--- client/src/pages/StartPage.tsx
+++ client/src/pages/StartPage.tsx
@@ -1,8 +1,12 @@
+import "./StartPage.scss"
+
 const StartPage = () => {
 
     return (
         <div className="StartPage">
-
+            <div className="NavTop">
+                <img className="Icon" src="/icons8-profile-48.png" alt="Profile"/>
+            </div>
         </div>
     )
 }
